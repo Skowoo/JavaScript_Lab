506a16a5cbb47d917bea80fcee07f72c2fb50e93
Skowoo <skowoo@tlen.pl>
2024-03-16 21:46:49 +0100

    arrows work

--- Projekt2/main.js
+++ Projekt2/main.js
@@ -1,33 +1,79 @@
-// const img = new Image()
-// const img = document.createElement('img')
-// img.src = 'url do zdjecia'
-
-// setInterval(() => {
-//     box.style.transform = 'tanslateX(300px)'
-// }, 20) // Set interval zapycha stack - ciągle wysyła żądanie
-
 const box = document.querySelectorAll('.box')
+const singleSlide = document.querySelector('.box')
+const leftButton = document.querySelector('.left')
+const rightButton = document.querySelector('.right')
 let position = 0
+let targetPosition = 0
+let currentIndex = 0
+let fullSize = (0.1 * singleSlide.clientWidth) + singleSlide.clientWidth // Full size (with spacer between slides)
 let speed = 20
 let interval = 10
 let direction = true
 
-const animuj = () => {
-    box.forEach(slide => {
-        slide.style.transform = 'translateX(' + position + 'px)'
-    })    
+box.forEach((slide, index) => { // set initial position
+    slide.style.transform = 'translateX(' + (position + (index * fullSize)) + 'px)';
+})
 
-    if (direction)
-        position += speed
-    else 
-        position -= speed
+rightButton.addEventListener('click', () => {
+    if (currentIndex === box.length - 1)
+    {
+        targetPosition = 0
+        currentIndex = 0
+        console.log('komenda w prawo')
+        goRight()
+    }        
+    else
+    {
+        targetPosition -= fullSize
+        currentIndex++
+        console.log('komenda w lewo')
+        goLeft()
+    }
+    console.log(targetPosition)
+    console.log(currentIndex)
+});
 
-    if (position < 0)
-        direction = true
-    else if (position > 1500)
-        direction = false
+leftButton.addEventListener('click', () => {
+    if (currentIndex === 0)
+    {
+        targetPosition = -1 * (box.length - 1) * fullSize
+        currentIndex = box.length - 1
+        console.log('komenda w lewo')
+        goLeft()
+    }        
+    else
+    {
+        targetPosition += fullSize
+        currentIndex--
+        console.log('komenda w prawo')
+        goRight()
+    }
+    console.log(targetPosition)
+    console.log(currentIndex)
+});
 
-    setTimeout(animuj, interval)
+const goRight = () => {    
+    box.forEach((slide, index) => {
+        slide.style.transform = 'translateX(' + (position + (index * fullSize)) + 'px)';
+    })
+
+    console.log('w prawo')
+
+    position += speed
+
+    if (position < targetPosition)
+        setTimeout(goRight, interval)
 }
 
-animuj()
+const goLeft = () => {    
+    box.forEach((slide, index) => {
+        slide.style.transform = 'translateX(' + (position + (index * fullSize)) + 'px)';
+    })
+
+    console.log('w lewo')
+
+    position -= speed
+
+    if (position > targetPosition)
+        setTimeout(goLeft, interval)
+}
