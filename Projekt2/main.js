72b2e1a4ffba08c3d2d3466abecbb8036f48f236
Skowoo <skowoo@tlen.pl>
2024-03-17 14:36:50 +0100

    movement tuning and automatisation with pause option

--- Projekt2/main.js
+++ Projekt2/main.js
@@ -1,22 +1,51 @@
 const box = document.querySelectorAll('.box')
 const singleSlide = document.querySelector('.box')
+const boxPlace = document.querySelector('.box_place')
 const leftButton = document.querySelector('.left')
 const rightButton = document.querySelector('.right')
 let position = 0
 let targetPosition = 0
 let currentIndex = 0
 let fullSize = (0.1 * singleSlide.clientWidth) + singleSlide.clientWidth // Full size (with spacer between slides)
-let speed = 20
-let interval = 10
-let direction = true
+let speed = 5
+let interval = 1
+let working = true
+
 
 box.forEach((slide, index) => { // set initial position
     slide.style.transform = 'translateX(' + (position + (index * fullSize)) + 'px)';
 })
 
+boxPlace.addEventListener('click', () =>{
+    if (working)
+    {
+        working = false
+    }        
+    else
+    {
+        working = true
+        autoMove()
+    }
+    console.log(working)
+})
+
 rightButton.addEventListener('click', () => {
+    working = true
+    commandRight()    
+})
+
+leftButton.addEventListener('click', () => {
+    working = true
+    commandLeft()
+})
+
+const commandRight = () =>{
+    if (!working)
+        return
+
     if (currentIndex === box.length - 1)
     {
+        speed = 50
         targetPosition = 0
         currentIndex = 0
         console.log('komenda w prawo')
@@ -24,6 +53,7 @@ rightButton.addEventListener('click', () => {
     }        
     else
     {
+        speed = 10
         targetPosition -= fullSize
         currentIndex++
         console.log('komenda w lewo')
@@ -31,18 +61,23 @@ rightButton.addEventListener('click', () => {
     }
     console.log(targetPosition)
     console.log(currentIndex)
-});
+}
 
-leftButton.addEventListener('click', () => {
+const commandLeft = () =>{
+    if (!working)
+        return
+
     if (currentIndex === 0)
     {
+        speed = 50
         targetPosition = -1 * (box.length - 1) * fullSize
-        currentIndex = box.length - 1
+        currentIndex = box.length - 1        
         console.log('komenda w lewo')
         goLeft()
     }        
     else
     {
+        speed = 10
         targetPosition += fullSize
         currentIndex--
         console.log('komenda w prawo')
@@ -50,14 +85,21 @@ leftButton.addEventListener('click', () => {
     }
     console.log(targetPosition)
     console.log(currentIndex)
-});
+}
 
-const goRight = () => {    
+const goRight = () => {
     box.forEach((slide, index) => {
         slide.style.transform = 'translateX(' + (position + (index * fullSize)) + 'px)';
     })
 
     console.log('w prawo')
+
+    if(targetPosition - position < 200)
+        speed = 3
+    else if(targetPosition - position < 100)
+        speed = 1
+    else if(targetPosition - position < 10)
+        speed = 0.2
 
     position += speed
 
@@ -72,8 +114,24 @@ const goLeft = () => {
 
     console.log('w lewo')
 
+    if(position - targetPosition < 200)
+        speed = 3
+    if(position - targetPosition < 100)
+        speed = 1
+    if(position - targetPosition < 10)
+        speed = 0.2
+
     position -= speed
 
     if (position > targetPosition)
         setTimeout(goLeft, interval)
 }
+
+const autoMove = () =>{
+    if (working){
+        commandRight()
+        setTimeout(autoMove, 5000)
+    }
+}
+
+autoMove()
