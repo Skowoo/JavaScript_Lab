546ee873c0d02372098604f7b0cfad68342e7b0e
Skowoo <skowoo@tlen.pl>
2024-04-06 15:48:50 +0200

    multi channel done

--- Projekt3/main.js
+++ Projekt3/main.js
@@ -29,45 +29,91 @@ metroOffButton.addEventListener('click', () => {
     metronomeInterval = undefined
 })
 
+// Multi channel:
+
+let channelCounter = 0
+const channelsBox = document.querySelector('.chanels_box')
+const addChannelButton = document.querySelector('#add_chanel_button')
+const existingChannels = []
+const playingChannels = []
+
 addEventListener('keypress',(ev)=>{
     if (ev.key in sounds){
         const sound = sounds[ev.key]
         sound.currentTime = 0
         sound.play()
-        if (isRecording){
-            recordedSounds.push([(Date.now() - recordBeganAt), ev.key])
-        }
+        checkAndRecord(ev.key)
     }
 })
 
-const recordButton = document.querySelector('#record_button')
-const stopRecordButton = document.querySelector('#stop_record_button')
-const playButton = document.querySelector('#play_button')
+addChannelButton.addEventListener('click', () => {
+    addChannel()
+})
 
-let recordBeganAt = Date.now()
-let recordEndAt = Date.now()
-let isRecording = false
-let recordedSounds = []
+const checkAndRecord = (key) => {
+    if (existingChannels.some(x => x[1]))
+    {
+        let activeChannels = existingChannels.filter(x => x[1])
+        activeChannels.forEach(element => {
+            element[4].push([(Date.now() - element[2]), key])
+        });
+    }
+}
 
-recordButton.addEventListener('click', () =>{
-    recordBeganAt = Date.now()
-    isRecording = true;
-} )
+const addChannel = () => {
+    var newChannel = document.createElement('div')
 
-stopRecordButton.addEventListener('click', () =>{
-    if (!isRecording)
-        recordedSounds = []
+    var startRecButton = document.createElement('button')
+    startRecButton.id = channelCounter
+    startRecButton.classList = 'btn start'
+    startRecButton.innerHTML = 'Nagrywaj'
+    newChannel.appendChild(startRecButton)
+    startRecButton.addEventListener('click', (ev) => {
+        existingChannels[ev.target.id][1] = true
+        existingChannels[ev.target.id][2] = Date.now()
+    })
 
-    recordEndAt = Date.now()
-    isRecording = false;
-} )
+    var stopRecButton = document.createElement('button')
+    stopRecButton.id = channelCounter
+    stopRecButton.classList = 'btn stop'
+    stopRecButton.innerHTML = 'Zakończ nagrywanie'
+    newChannel.appendChild(stopRecButton)
+    stopRecButton.addEventListener('click', (ev) => {
+        existingChannels[ev.target.id][1] = false
+        existingChannels[ev.target.id][3] = Date.now() - existingChannels[ev.target.id][2] // Length of record
+    })
 
-playButton.addEventListener('click', () =>{
-    recordedSounds.forEach(element => {
-        setTimeout(() => { 
-            const sound = sounds[element[1]]
-            sound.currentTime = 0
-            sound.play()
-        }, element[0])
-    });
-} )
+    var playButton = document.createElement('button')
+    playButton.id = channelCounter
+    playButton.classList = 'btn play'
+    playButton.innerHTML = 'Odtwarzaj'
+    newChannel.appendChild(playButton)
+    playButton.addEventListener('click', (ev) => {
+        console.log(existingChannels[ev.target.id][3] + 100)
+        playingChannels.push([ev.target.id, setInterval(
+            existingChannels[ev.target.id][4].forEach(element => {            
+                setTimeout(() => { 
+                    const sound = sounds[element[1]]
+                    sound.currentTime = 0
+                    sound.play()
+                }, element[0])
+            }), (existingChannels[ev.target.id][3] + 100)
+        )])
+    })
+
+    var stopPlayButton = document.createElement('button')
+    stopPlayButton.id = channelCounter
+    stopPlayButton.classList = 'btn stop_play'
+    stopPlayButton.innerHTML = 'Zatrzymaj odtwarzanie'
+    newChannel.appendChild(stopPlayButton)
+    stopPlayButton.addEventListener('click', (ev) => {
+        
+    })
+
+    existingChannels.push([channelCounter, false, 0, 0, []])    
+
+    channelsBox.appendChild(newChannel)
+    channelsBox.appendChild(document.createElement('br'))
+
+    channelCounter++
+}
