8d8e6307663eaf4af56d78a5bdcec9655e71eb80
Skowoo <skowoo@tlen.pl>
2024-04-06 12:18:41 +0200

    TBD: dynamic channels

--- Projekt3/main.js
+++ Projekt3/main.js
@@ -1,6 +1,3 @@
-// const handle = setInterval(() => {}, 2000)
-// clearInterval(handle) // Stopping intervals
-
 const sounds = {
     'a': document.querySelector('#clap'),
     's': document.querySelector('#kick'),
@@ -17,8 +14,8 @@ const recordButton = document.querySelector('#record_button')
 const stopRecordButton = document.querySelector('#stop_record_button')
 const playButton = document.querySelector('#play_button')
 const metroOnButton = document.querySelector('#metro_on_button')
-const metroOffButton = document.querySelector('#metro_input')
-const metroInput = document.querySelector('#play_button')
+const metroOffButton = document.querySelector('#metro_off_button')
+const metroInput = document.querySelector('#metro_input')
 
 let recordBeganAt = Date.now()
 let recordEndAt = Date.now()
@@ -26,26 +23,29 @@ let isRecording = false
 let recordedSounds = []
 let metronomeInterval
 
-
-addEventListener('keypress',(ev)=>{
-    const sound = sounds[ev.key]
-    sound.currentTime = 0
-    sound.play()
-    if (isRecording){
-        recordedSounds.push([(Date.now() - recordBeganAt), ev.key])
+metroOnButton.addEventListener('click', () => {
+    if(metronomeInterval === undefined){
+        metronomeInterval = setInterval(() =>{
+            const sound = sounds['k']
+            sound.currentTime = 0
+            sound.play()} , 60000 / metroInput.value)
     }
 })
 
-metroOnButton.addEventListener('click', () => {
-    metronomeInterval = setInterval(() =>{
-        const sound = sounds['k']
-        sound.currentTime = 0
-        sound.play()
-    } , metroInput.value)
+metroOffButton.addEventListener('click', () => {
+    clearInterval(metronomeInterval)
+    metronomeInterval = undefined
 })
 
-metroOffButton.addEventListener('click', () => {
-    clearInterval(metronomeInterval);
+addEventListener('keypress',(ev)=>{
+    if (ev.key in sounds){
+        const sound = sounds[ev.key]
+        sound.currentTime = 0
+        sound.play()
+        if (isRecording){
+            recordedSounds.push([(Date.now() - recordBeganAt), ev.key])
+        }
+    }
 })
 
 recordButton.addEventListener('click', () =>{
@@ -65,9 +65,6 @@ playButton.addEventListener('click', () =>{
     console.log(recordedSounds)
     play()
 } )
-
-
-
 
 let play = () => {
     recordedSounds.forEach(element => {
