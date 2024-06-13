0d8e9a729c9404ee14e01b5dfd88e3b2e9e4da77
Skowoo <skowoo@tlen.pl>
2024-06-13 12:30:09 +0200

    BiedAgar done (single mode)

--- Agar/main.js
+++ Agar/main.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-let gravityForce
-let mouseOnCanvas = false
 let mousePosX
 let mousePosY
 let foodBalls = []
@@ -15,13 +13,19 @@ function trackMouse(e) {
 }
 
 class Ball {
-    constructor(x, y, speed, mass, colorArray) {
+    constructor(x, y, speed, mass, color) {
         this.x = x;
         this.y = y;
         this.vx = speed * (Math.random() - 0.5);
         this.vy = speed * (Math.random() - 0.5);
-        this.colorArray = colorArray;
+        this.color = color;
         this.mass = mass
+    }
+
+    activate(){
+        this.move()
+        this.eat()
+        this.draw()   
     }
 
     move() {
@@ -34,7 +38,7 @@ class Ball {
             let dx = this.x - foodBalls[i].x;
             let dy = this.y - foodBalls[i].y;
             let dist = Math.sqrt(dx * dx + dy * dy);
-            if (dist < (50 +(this.mass / Math.PI))) {
+            if (dist < (10 + (this.mass / Math.PI) + (foodBalls[i].mass / Math.PI))) {
                 if (this.mass <= foodBalls[i].mass)
                     flow(this, foodBalls[i], dist)
                 else
@@ -46,48 +50,47 @@ class Ball {
     draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, Math.abs((this.mass / Math.PI)), 0, Math.PI * 2);
-        ctx.fillStyle = 'rgba('+ (this.colorArray[0]) + ',' + (this.colorArray[1]) + ',' + (this.colorArray[2]) + ',1)';
+        ctx.fillStyle = this.color
         ctx.fill();
         ctx.closePath();
     }
 }
 
 function flow(smaller, bigger, dist){
-    ctx.strokeStyle = 'rgba('+ 
-    bigger.colorArray[0] + ',' + 
-    bigger.colorArray[1] + ',' + 
-    bigger.colorArray[2] + ',' + 
-    smaller.mass / 30 +')';
+    ctx.strokeStyle = bigger.color
     ctx.beginPath();
     ctx.moveTo(bigger.x, bigger.y);
     ctx.lineTo(smaller.x, smaller.y);
     ctx.stroke();
     smaller.mass--
-    bigger.mass += (1 / dist)
+    bigger.mass += (1 / 10)
     if (smaller.mass < 1){
         replaceBall(smaller)
     }
 }
 
 function start() {
-    ctx.canvas.width  = 0.98 * window.innerWidth;
-    ctx.canvas.height = 0.97 * window.innerHeight;
+    ctx.canvas.width  = 0.95 * window.innerWidth;
+    ctx.canvas.height = 0.95 * window.innerHeight;
     
     mousePosX = canvas.width / 2
     mousePosY = canvas.height / 2
-    player = new Ball(mousePosX, mousePosY, 1, 25, [(Math.random() * 255), (Math.random() * 255), (Math.random() * 255),])
+    player = new Ball(mousePosX, mousePosY, 1, 25, getRandomColor())
     addFood((canvas.width + canvas.height) / 10)
 
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         for (const ball of foodBalls) {
             ball.draw();
+        }     
+        player.activate()
+
+        if (player.mass < 1){
+            cancelAnimationFrame(animation);
+            ctx.font = "50px Arial";
+            ctx.fillText("Porażka!", (canvas.width / 2) - 50, canvas.height / 2);
         }
 
-        player.move()
-        player.eat()
-        player.draw()        
-        
         animation = requestAnimationFrame(animate);
     }
     animate();
@@ -97,7 +100,7 @@ function addFood(quantity){
     for (let i = 0; i < quantity; i++) {
         let x = Math.random() * (canvas.width - 2 * 5) + 5;
         let y = Math.random() * (canvas.height - 2 * 5) + 5;
-        let color = [(Math.random() * 255), (Math.random() * 255), (Math.random() * 255),]
+        let color = getRandomColor()
         foodBalls.push(new Ball(x, y, 0, (Math.random() * (player.mass)), color));
     }
 }
@@ -105,7 +108,11 @@ function addFood(quantity){
 function replaceBall(ball){
     ball.x = Math.random() * (canvas.width - 2 * 5) + 5;
     ball.y = Math.random() * (canvas.height - 2 * 5) + 5;
-    ball.mass = (Math.random() * (player.mass * 1.2))
+    ball.mass = (Math.random() * (player.mass * 1.1))
+}
+
+function getRandomColor(){
+    return 'rgba('+ Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ',1)'
 }
 
 start()
